9a6f6b1981ef73f5b1af06b2731d6926191ffbc5
Jagger Schmitz <jschmitz789@gmail.com>
2025-02-12 19:17:28 -0600

    Created library class and addBook

--- src/project01/library/Library.java
+++ src/project01/library/Library.java
@@ -1,5 +1,39 @@
 package project01.library;
+import project01.book.*;
 
-public class Library {
-
+/**
+ * create library class to manage array of Book objects
+ */
+public class Library 
+{
+	private Book[] books;
+	private int bookcount;
+	
+/**
+ * initialize library w fixed size of 5 and private int count to keep track of
+ * number of books
+ */
+public Library()
+{
+	books = new Book[5];
+	bookcount = 0;
 }
+
+/**
+ * used to add book to library
+ * @param book the Book to add
+ * @return true if book is added and false if libraries full
+ */
+public boolean addBook(Book book)
+{
+	if(bookcount<books.length)
+	{
+		books[bookcount]=book;
+		bookcount++;
+		return true;
+	}
+		return false;
+}
+}
+
+
